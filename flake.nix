4e806509538f29afdb995d08f2f543809cf8cfe2
Ian Glen Neal <ian.gl.neal@gmail.com>
2024-09-10 15:48:15 -0400

    Use veridise-pkgs for nix

--- flake.nix
+++ flake.nix
@@ -1,69 +1,27 @@
 {
   inputs = {
-    flake-utils = {
-      url = "github:numtide/flake-utils/v1.0.0";
+    veridise-pkgs = {
+      url = "git+ssh://git@github.com/Veridise/veridise-nix-pkgs.git?ref=bryan/mlir";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   # Custom colored bash prompt
   nixConfig.bash-prompt = ''\[\e[0;32m\][ZKIR]\[\e[m\] \[\e[38;5;244m\]\w\[\e[m\] % '';
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, veridise-pkgs, }:
     {
       # First, we define the packages used in this repository/flake
       overlays.default = final: prev: {
-        # Use a custom version of LLVM to
-        # 1) enable some features we need
-        # 2) speed up the build
-        # 3) add MLIR (which is not packaged in nixpkgs yet)
-        zkir_llvm =
-          let
-            lpkgs = final.llvmPackages_18;
-            tools = lpkgs.tools.extend (tpkgs: tpkgsOld: {
-              libllvm = (tpkgsOld.libllvm.override ({
-                # Skip tests since they take a long time to build and run
-                doCheck = false;
-              })).overrideAttrs (attrs: {
-                cmakeFlags = attrs.cmakeFlags ++ [
-                  # Skip irrelevant targets
-                  "-DLLVM_TARGETS_TO_BUILD=host"
-                  "-DLLVM_INCLUDE_BENCHMARKS=OFF"
-                  "-DLLVM_INCLUDE_EXAMPLES=OFF"
-                  "-DLLVM_INCLUDE_TESTS=OFF"
-                  # Need the following to enable exceptions
-                  "-DLLVM_ENABLE_EH=ON"
-                  # Assertions are very useful for debugging
-                  "-DLLVM_ENABLE_ASSERTIONS=ON"
-                ];
-              });
-
-              mlir = final.callPackage ./nix/mlir/default.nix {
-                inherit (tpkgs.libllvm) monorepoSrc version;
-                buildLlvmTools = final.llvmLocal;
-                llvm_meta = lpkgs.libllvm.meta;
-                inherit (tpkgs) libllvm;
-
-                debugVersion = true;
-
-                # stdenv = final.ccacheStdenv;
-                #
-                # Unfortunately, ccache is currently broken due to
-                # https://github.com/NixOS/nixpkgs/issues/119779
-              };
-
-              mlirWithPython = final.mlir.override {
-                enablePythonBindings = true;
-              };
-            });
-          in
-            lpkgs // { inherit tools; } // tools;
-
-        inherit (final.zkir_llvm) mlir mlirWithPython;
+        mlirWithPython = final.mlir.override {
+          enablePythonBindings = true;
+        };
 
         zkir = final.callPackage ./nix/zkir.nix {};
         zkirWithPython = final.zkir.override {
           mlir = final.mlirWithPython;
         };
+
         zkirDebugClang = (final.zkir.override { stdenv = final.clangStdenv; }).overrideAttrs(attrs: {
           cmakeBuildType = "DebWithSans";
 
@@ -93,7 +51,7 @@
             llvm-cov export -format=lcov -instr-profile $PROFDATA $OBJS > report.lcov
             rm -rf $MANIFEST $PROFDATA $BINS
           '';
-          
+
           postInstall = ''
             mkdir -p $out/artifacts/
             echo "-- Copying coverage summary to $out/artifacts/cov-summary.txt"
@@ -130,7 +88,7 @@
         # Because `nix develop` is used to set up a dev shell for a given
         # derivation, we just need to extend the zkir derivation with any
         # extra tools we need.
-        devShellBase = { pkgs, zkirEnv ? final.zkir }: {
+        devShellBase = { pkgs, zkirEnv ? final.zkir, ... }: {
           shell = zkirEnv.overrideAttrs (old: {
             nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs; [
               doxygen
@@ -160,7 +118,10 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
+          overlays = [
+            self.overlays.default
+            veridise-pkgs.overlays.default
+          ];
         };
       in
       {
@@ -186,8 +147,8 @@
             # Use Debug by default so assertions are enabled by default.
             cmakeBuildType = "Debug";
           });
-          debugClang = (pkgs.devShellBase pkgs pkgs.zkirDebugClang).shell;
-          debugGCC = (pkgs.devShellBase pkgs pkgs.zkirDebugGCC).shell;
+          debugClang = _: (pkgs.devShellBase pkgs pkgs.zkirDebugClang).shell;
+          debugGCC = _: (pkgs.devShellBase pkgs pkgs.zkirDebugGCC).shell;
 
           llvm = pkgs.mkShell {
             buildInputs = [ pkgs.zkir_llvm.tools.libllvm.dev ];
