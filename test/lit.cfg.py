18f1e6d9abff9ec2be00b16d292098ba9f37e466
Ian Glen Neal <ian.gl.neal@gmail.com>
2024-09-11 13:00:52 -0400

    Undo autoformat

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -14,57 +14,55 @@ import tempfile
 
 import lit.formats
 import lit.util
+
 from lit.llvm import llvm_config
-from lit.llvm.subst import FindTool, ToolSubst
+from lit.llvm.subst import ToolSubst
+from lit.llvm.subst import FindTool
 
 # Configuration file for the 'lit' test runner.
 
 # name: The name of this test suite.
-config.name = "ZKIR"
+config.name = 'ZKIR'
 
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = [".mlir", ".zkir"]
+config.suffixes = ['.mlir', '.zkir']
 config.suffixes.extend(config.extra_suffixes)
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.zkir_obj_root, "test")
+config.test_exec_root = os.path.join(config.zkir_obj_root, 'test')
 
-config.substitutions.append(("%PATH%", config.environment["PATH"]))
+config.substitutions.append(('%PATH%', config.environment['PATH']))
 
-llvm_config.with_system_environment(["HOME", "INCLUDE", "LIB", "TMP", "TEMP"])
+llvm_config.with_system_environment(
+    ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
 
 llvm_config.use_default_substitutions()
 
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
 # directories.
-config.excludes = ["Inputs", "Examples", "CMakeLists.txt", "README.txt", "LICENSE.txt"]
+config.excludes = ['Inputs', 'Examples', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.zkir_obj_root, "test")
-config.zkir_tools_dir = os.path.join(config.zkir_obj_root, "bin")
+config.test_exec_root = os.path.join(config.zkir_obj_root, 'test')
+config.zkir_tools_dir = os.path.join(config.zkir_obj_root, 'bin')
 
 # Tweak the PATH to include the tools dir.
-llvm_config.with_environment("PATH", config.zkir_tools_dir, append_path=True)
-llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
+llvm_config.with_environment('PATH', config.zkir_tools_dir, append_path=True)
+llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 
 tool_dirs = [config.zkir_tools_dir, config.llvm_tools_dir]
-tools = ["zkir-opt"]
+tools = [
+    "zkir-opt"
+]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
 
-llvm_config.with_environment(
-    "PYTHONPATH",
-    [
-        os.path.join(config.mlir_obj_dir, "python_packages", "zkir"),
-    ],
-    append_path=True,
-)
-
-if config.per_test_coverage:
-    config.environment["LLVM_PROFILE_FILE"] = "covdata-%p.profraw"
+llvm_config.with_environment('PYTHONPATH', [
+    os.path.join(config.mlir_obj_dir, 'python_packages', 'zkir'),
+], append_path=True)
