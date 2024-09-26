f83e1c389a78c066f8daee74472564a7bb5cdba5
Tim Hoffman <timothy.hoffman@veridise.com>
2024-09-26 10:16:52 -0500

    add dependent dialects

--- include/Dialect/ZKIR/IR/Dialect.h
+++ include/Dialect/ZKIR/IR/Dialect.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <mlir/IR/Dialect.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/Index/IR/IndexDialect.h>
 
 // Include TableGen'd files
 #include "Dialect/ZKIR/IR/Dialect.h.inc"
